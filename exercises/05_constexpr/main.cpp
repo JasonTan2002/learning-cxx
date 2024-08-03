bf1cacc5cc612895dadab48051c210cf3c835d53
JustFallInLove <595355101@qq.com>
2024-08-03 11:49:07 +0800

    code optimization

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,14 +1,27 @@
 #include "../exercise.h"
 
 constexpr unsigned long long fibonacci(int i) {
-    switch (i) {
-        case 0:
-            return 0;
-        case 1:
-            return 1;
-        default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+    // switch (i) {
+    //     case 0:
+    //         return 0;
+    //     case 1:
+    //         return 1;
+    //     default:
+    //         return fibonacci(i - 1) + fibonacci(i - 2);
+    // }
+    //dynamic programming
+    if (i == 0) return 0;
+    if (i == 1) return 1;
+
+    unsigned long long prev1 = 0, prev2 = 1;
+    unsigned long long result = 0;
+
+    for (int j = 2; j <= i; ++j) {
+        result = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = result;
     }
+    return result;
 }
 
 int main(int argc, char **argv) {
